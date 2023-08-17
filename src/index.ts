bdc55ffe9fb3f7bb47888599dc0755a9a425e36c
fikayo <fikayoadele@gmail.com>
2023-08-17 15:50:25 +0100

    server location

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,11 @@ import express, { Express, Request, Response } from 'express';
 const app: Express = express();
 import dotenv from 'dotenv';
 dotenv.config();
-// require('dotenv').config();
-// require('express-async-errors');
+import axios from 'axios';
 const cors = require('cors');
-// const notFoundMiddleware = require('./middleware/not-found');
-// const errorHandlerMiddleware = require('./middleware/error-handler');
-// const connectDB = require('./db/connect');
+import { StatusCodes } from 'http-status-codes';
 const cookieParser = require('cookie-parser');
-
+import { authUser } from './api/middlewares/auth';
 app.use(cors());
 app.use(express.json());
 
@@ -18,10 +15,10 @@ const orderRoute = require('./api/routes/orderRoutes')
 const foodRoute = require('./api/routes/foodRoutes')
 // rest of the packages
 //const morgan = require('morgan');
-//const fileUpload = require('express-fileupload');
+
 //const rateLimiter = require('express-rate-limit');
 //const helmet = require('helmet');
-//const xss = require('xss-clean');
+
 
 // app.use(notFoundMiddleware);
 // app.use(errorHandlerMiddleware);
@@ -33,6 +30,21 @@ app.use('/api/v1/food',foodRoute)
 // app.use('/api/v1/post', postRoute);
 // app.use('/api/v1/notification', notificationRoute);
 // app.use('/api/v1/news', newsRoute);
+
+app.get('/api/v1/server-location', async (req, res) => {
+  try {
+    const response = await axios.get('http://ip-api.com/json');
+    const serverLocation = {
+      latitude: response.data.lat,
+      longitude: response.data.lon,
+    };
+    res.status(StatusCodes.OK).json(serverLocation);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json('could not get server location');
+    }
+  }
+});
 
 const port = process.env.PORT || 5000;
 const start =  () => {
